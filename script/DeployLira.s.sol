de0e4053d9dee60430127b033443891e4e4082eb
Odin Thif <mjd.thif@icloud.com>
2025-07-11 00:00:18 +0200

    #12 deploy the lira token along with lira engine that needs further more configration to be working with several chains(networks)

--- script/DeployLira.s.sol
+++ script/DeployLira.s.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {Lira} from "../src/Lira.sol";
+import {LiraEngine} from "../src/LiraEngine.sol";
 
 contract DeployLira is Script {
-    function setUp() public {}
-
-    function run() public {
+        function run() external returns (Lira, LiraEngine) {
         vm.startBroadcast();
-        new Lira();
+Lira lira =         new Lira();
+//address[] memory collateralAddresses, address[] memory priceFeedAddresses, address liraTokenAddress
+        LiraEngine liraEngine = new LiraEngine(,,lira.address());
         vm.stopBroadcast();
     }
 }
